7139e672d75f0cc817eaa728cf85519fee064061
Keiko M <vikakeiko21@gmail.com>
2019-07-28 10:18:33 -0700

    orm methods

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,8 @@
-​-- Drops the wishes_db if it already exists --
-DROP DATABASE IF EXISTS burgers_db;
-​
--- Create the database wishes_db and specified it for use.
+###Schema
+
 CREATE DATABASE burgers_db;
-​
 USE burgers_db;
-​
--- Create the table wishes.
+
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
