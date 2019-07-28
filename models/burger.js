27a138cc98869a02c7e3650b2797c2c28aab397d
Keiko M <vikakeiko21@gmail.com>
2019-07-28 10:23:04 -0700

    model setup

--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,30 @@ var orm = require("../config/orm.js");
 
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    deleteOne: function (condition, cb) {
+        orm.deleteOne("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (controller.js).
+module.exports = burger;
